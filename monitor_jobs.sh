3f749f5382056fc3fc26eb6e0f2542f25ca05481
karllmitchell <karl.mitchell@me.com>
2017-02-16 16:09:29 -0800

    For monitoring GNU parallel transcoding
    Opens a terminal for each job if WORKING_DIR is specified as the first command, e.g. "monitor_jobs.sh /mnt/dvr/tmp"

--- monitor_jobs.sh
+++ monitor_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Allows Channels-DVR-to-Plex jobs run with parallels to be monitored in real-time
+# Allows Channels-DVR-to-Plex jobs run with GNU parallels to be monitored in real-time
 # Usage: monitor_jobs.sh ${WORKING_DIR}
 
 find "${1} -name stdout -exec xterm -e tail -f {} \;
